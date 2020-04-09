267532a89b890f62ea0139a93e805b991be8f9ff
Realizedd <TheMCman12@gmail.com>
2020-04-09 12:47:10 +0900

    add 1.14 module

---
+++ v1_14_R1/src/main/java/me/realized/de/arenaregen/nms/v1_14_R1/NMSHandler.java
@@ -0,0 +1,48 @@
+package me.realized.de.arenaregen.nms.v1_14_R1;
+
+import me.realized.de.arenaregen.nms.NMS;
+import net.minecraft.server.v1_14_R1.BlockPosition;
+import net.minecraft.server.v1_14_R1.Chunk;
+import net.minecraft.server.v1_14_R1.IBlockData;
+import net.minecraft.server.v1_14_R1.World;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.craftbukkit.v1_14_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_14_R1.util.CraftMagicNumbers;
+
+public class NMSHandler implements NMS {
+
+    @Override
+    public void setBlockFast(final Block bukkitBlock, final Material material, final int data) {
+        final int x = bukkitBlock.getX(), y = bukkitBlock.getY(), z = bukkitBlock.getZ();
+        final BlockPosition position = new BlockPosition(x, y, z);
+        final Chunk chunk = ((CraftChunk) bukkitBlock.getChunk()).getHandle();
+        final net.minecraft.server.v1_14_R1.Block block = CraftMagicNumbers.getBlock(material);
+        final IBlockData blockData = block.getBlockData();
+        chunk.setType(position, blockData, true);
+
+        if (bukkitBlock.getType() == Material.AIR || isSurrounded(bukkitBlock)) {
+            return;
+        }
+
+        final World world = ((CraftWorld) bukkitBlock.getWorld()).getHandle();
+        world.getChunkProvider().getLightEngine().a(position);
+    }
+
+    private boolean isSurrounded(final Block block) {
+        final Block east = block.getRelative(BlockFace.EAST);
+        final Block west = block.getRelative(BlockFace.WEST);
+        final Block south = block.getRelative(BlockFace.SOUTH);
+        final Block north = block.getRelative(BlockFace.NORTH);
+        final Block up = block.getRelative(BlockFace.UP);
+        final Block down = block.getRelative(BlockFace.DOWN);
+        return !east.getType().isTransparent()
+            && !west.getType().isTransparent()
+            && !up.getType().isTransparent()
+            && !down.getType().isTransparent()
+            && !south.getType().isTransparent()
+            && !north.getType().isTransparent();
+    }
+}
