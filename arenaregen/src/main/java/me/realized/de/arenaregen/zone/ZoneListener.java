07c44f61aab9bfa9ee29fc1f55989f0f2e2fd444
Realizedd <TheMCman12@gmail.com>
2022-04-08 23:54:39 +0900

    TEST: Temp disable explosion listening

--- arenaregen/src/main/java/me/realized/de/arenaregen/zone/ZoneListener.java
+++ arenaregen/src/main/java/me/realized/de/arenaregen/zone/ZoneListener.java
@@ -181,45 +181,45 @@ public class ZoneListener implements Listener {
     }
 
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void on(final EntityExplodeEvent event) {
-        final Zone zone = zoneManager.get(event.getEntity().getLocation().getBlock());
+    // @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    // public void on(final EntityExplodeEvent event) {
+    //     final Zone zone = zoneManager.get(event.getEntity().getLocation().getBlock());
 
-        if (zone == null) {
-            return;
-        }
+    //     if (zone == null) {
+    //         return;
+    //     }
 
-        if (config.isTrackBlockChanges()) {
-            zone.track(event.blockList());
-        }
+    //     if (config.isTrackBlockChanges()) {
+    //         zone.track(event.blockList());
+    //     }
 
-        if (!config.isPreventBlockExplode()) {
-            return;
-        }
+    //     if (!config.isPreventBlockExplode()) {
+    //         return;
+    //     }
 
-        event.setCancelled(true);
-    }
+    //     event.setCancelled(true);
+    // }
 
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void on(final BlockExplodeEvent event) {
-        final Block block = event.getBlock();
-        final Zone zone = zoneManager.get(block);
+    // @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    // public void on(final BlockExplodeEvent event) {
+    //     final Block block = event.getBlock();
+    //     final Zone zone = zoneManager.get(block);
 
-        if (zone == null) {
-            return;
-        }
+    //     if (zone == null) {
+    //         return;
+    //     }
 
-        if (config.isTrackBlockChanges()) {
-            zone.track(event.blockList());
-        }
+    //     if (config.isTrackBlockChanges()) {
+    //         zone.track(event.blockList());
+    //     }
 
-        if (!config.isPreventBlockExplode()) {
-            return;
-        }
+    //     if (!config.isPreventBlockExplode()) {
+    //         return;
+    //     }
 
-        event.setCancelled(true);
-    }
+    //     event.setCancelled(true);
+    // }
 
     
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
